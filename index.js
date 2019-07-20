cbf7ddd9c1d12a9b60dcf4384d54f22e19430b10
BetDex <major058@umn.edu>
2019-07-20 14:40:43 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//https://github.com/desmondmorris/node-twitter/tree/master/examples#tweet
-
 const Twitter = require("twitter")
 const dotenv = require("dotenv")
 
@@ -12,54 +10,43 @@ const client = new Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
-//tweet with only text
-function publishTextPost() {
-  client.post("statuses/update", { status: "I tweeted from Node.js!" }, function(error, tweet, response) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(tweet)
+// Tweet with Text
+client.post("statuses/update", { status: "I tweeted from Node.js!" }, function(error, tweet, response) {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log(tweet)
+  }
+})
+
+//Tweet with Text and Image
+const imageData = fs.readFileSync("./media/george.jpg") //replace with the path to your image
+
+client.post("media/upload", {media: imageData}, function(error, media, response) {
+  if (error) {
+    console.log(error)
+  } else {
+    const status = {
+      status: "I tweeted from Node.js!",
+      media_ids: media.media_id_string
     }
-  })
-}
 
-//tweet with image
-  //upload image
-  //post tweet including image
-const imageData = require("fs").readFileSync("./media/george.jpg")
-
-function publishImagePost() {
-  client.post("media/upload", {media: imageData}, function(error, media, response) {
-    if (error) {
-      console.log("media upload")
-      console.log(error)
-    } else {
-      console.log(media)
-
-      const status = {
-        status: "Tweet text.",
-        media_ids: media.media_id_string
+    client.post("statuses/update", status, function(error, tweet, response) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log("Successfully tweeted an image!")
       }
+    })
+  }
+})
 
-      client.post("statuses/update", status, function(error, tweet, response) {
-        if (error) {
-          console.log(error)
-        } else {
-          console.log(tweet)
-        }
-      })
-    }
-  })
-}
+//Tweet with Media (GIF or Video)
+const pathToFile = "./media/homer.gif" //for video: "./media/test.mp4"
+const mediaType = "image/gif" //for video: "video/mp4"
 
-//tweet with gif or mp4 video
-const pathToGif = "./media/homer.gif"
-const pathToVideo = "./media/test.mp4"
-
-const mediaType = "video/mp4" //change to "image/gif" for a gif file
-
-const mediaData = require("fs").readFileSync(pathToVideo)
-const mediaSize = require("fs").statSync(pathToVideo).size
+const mediaData = fs.readFileSync(pathToFile)
+const mediaSize = fs.statSync(pathToFile).size
 
 initializeMediaUpload()
   .then(appendFileChunk)
@@ -68,7 +55,11 @@ initializeMediaUpload()
 
 function initializeMediaUpload() {
   return new Promise(function(resolve, reject) {
-    client.post("media/upload", {command: "INIT", total_bytes: mediaSize, media_type : mediaType}, function(error, data, response) {
+    client.post("media/upload", {
+      command: "INIT",
+      total_bytes: mediaSize,
+      media_type: mediaType
+    }, function(error, data, response) {
       if (error) {
         console.log(error)
         reject(error)
@@ -81,7 +72,12 @@ function initializeMediaUpload() {
 
 function appendFileChunk(mediaId) {
   return new Promise(function(resolve, reject) {
-    client.post("media/upload", {command: "APPEND", media_id: mediaId, media: mediaData, segment_index: 0}, function(error, data, response) {
+    client.post("media/upload", {
+      command: "APPEND",
+      media_id: mediaId,
+      media: mediaData,
+      segment_index: 0
+    }, function(error, data, response) {
       if (error) {
         console.log(error)
         reject(error)
@@ -94,7 +90,10 @@ function appendFileChunk(mediaId) {
 
 function finalizeUpload(mediaId) {
   return new Promise(function(resolve, reject) {
-    client.post("media/upload", {command: "FINALIZE", media_id: mediaId}, function(error, data, response) {
+    client.post("media/upload", {
+      command: "FINALIZE",
+      media_id: mediaId
+    }, function(error, data, response) {
       if (error) {
         console.log(error)
         reject(error)
@@ -106,18 +105,18 @@ function finalizeUpload(mediaId) {
 }
 
 function publishStatusUpdate(mediaId) {
-  const status = {
-    status: "Homer gif from Node.js.",
-    media_ids: mediaId
-  }
-
-  client.post("statuses/update", status, function(error, data, response) {
-    if (error) {
-      console.log(error)
-      reject(error)
-    } else {
-      console.log("Successfully uploaded media and tweeted.")
-      return data
-    }
+  return new Promise(function(resolve, reject) {
+    client.post("statuses/update", {
+      status: "I tweeted from Node.js!",
+      media_ids: mediaId
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        console.log("Successfully uploaded media and tweeted!")
+        resolve(data)
+      }
+    })
   })
 }
